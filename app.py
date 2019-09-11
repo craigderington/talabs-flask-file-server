89b256ac33da2c886fa486f2f559c72d14e38fa5
Craig Derington <craig@craigderington.me>
2019-09-11 15:45:01 -0500

    Update app.py, add templates and error handlers.

--- app.py
+++ app.py
@@ -3,18 +3,21 @@
 import logging
 import os
 import config
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, render_template
+from datetime import datetime, timedelta
 
-SECRET_KEY = config.SECRET_KEY
 app = Flask(__name__,
-            static_folder="static")
+            static_folder="static",
+            template_folder="templates")
 
 gunicorn_logger = logging.getLogger("gunicorn.error")
 app.logger.handlers = gunicorn_logger.handlers
 app.logger.setLevel(gunicorn_logger.level)
+app.SECRET_KEY = config.SECRET_KEY
 
 
 @app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
 def index():
     """
     Default site index placeholder
@@ -22,10 +25,10 @@ def index():
     :return str
     """
     app.logger.info("Serving {} request on site index".format(request.method))
-    msg = "Download files from the static directory.  \
-           Usage:  http://server-ip:8000/filename.ext"
-    
-    return msg
+    return render_template(
+        "index.html",
+        today=get_date()
+    )
 
 
 @app.route("/<path:filename>", methods=["GET"])
@@ -41,6 +44,36 @@ def send_file(filename):
         app.static_folder + "/docs/",
         filename
     )
+
+
+@app.route("/login", methods=["GET"])
+def login():
+    """
+    Render the login page
+    """
+    return render_template(
+        "login.html",
+        today=get_date()
+    )
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(err):
+    return render_template('500.html'), 500
+
+
+def get_date():
+    """
+    Generate the current date time
+    :param: none
+    :return date as str
+    """
+    return datetime.now().strftime("%c")
 
 
 if __name__ == "__main__":   
