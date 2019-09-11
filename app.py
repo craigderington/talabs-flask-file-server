e68d331372a1bd60900888c347dc2c358c36ffd5
Craig Derington <craig@craigderington.me>
2019-09-11 14:42:26 -0500

    Update app.py with logging.

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
-#!.env/bin/python
+#!usr/bin/python
 
 import os
 from flask import Flask, request, send_from_directory
-
-app = Flask(__name__,
-            static_folder="static")
+import logging
 
 SECRET_KEY = os.urandom(64)
 DEBUG = True
 
+app = Flask(__name__,
+            static_folder="static")
+
+
 @app.route("/", methods=["GET"])
 def index():
     """
-    Homepage
-    :pass
-    :return
+    Default site index placeholder
+    :param none
+    :return str
     """
-    return "Hello, World!"
+    app.logger.info("Serving {} request on site index".format(request.method))
+    msg = "Download files from the static directory.  \
+           Usage:  http://server-ip:8000/filename.ext"
+    return msg
 
 
 @app.route("/<path:filename>", methods=["GET"])
 def send_file(filename):
     """
-    Expose an endpoint and serve files
-    :param
-    :return 
+    Expose an endpoint and serve static files
+    :param filename
+    :return file
     """
+    app.logger.info("Serving {} request for file: {}".format(request.method, filename))
+
     return send_from_directory(
         app.static_folder,
         filename
     )
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
     app.run(
         host="0.0.0.0",
         port=8000,
